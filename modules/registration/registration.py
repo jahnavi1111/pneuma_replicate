dd103247ed08c01548ec6e4882c014f3aef5fd47
David Alexander <david.alexander01@ui.ac.id>
2024-07-05 16:38:24 +0700

    load csv into database

--- modules/registration/registration.py
+++ modules/registration/registration.py
@@ -1,4 +1,26 @@
 import duckdb
+import fire
 
-cursor = duckdb.connect()
-print(cursor.execute("SELECT 42").fetchall())
+
+class Registration:
+    def __init__(self, db_path: str):
+        self.db_path = db_path
+
+    def read_csv(self, csv_path: str):
+        name = csv_path.split("/")[-1][:-4]
+        print(name)
+        with duckdb.connect(self.db_path) as con:
+            con.sql(
+                f"""CREATE TABLE {name} AS
+                FROM read_csv(
+                    '{csv_path}',
+                    auto_detect=True,
+                    header=True
+                )"""
+            )
+            print(con.sql("SHOW TABLES"))
+            print(con.sql("SELECT * FROM BX1_chicago"))
+
+
+if __name__ == "__main__":
+    fire.Fire(Registration)
