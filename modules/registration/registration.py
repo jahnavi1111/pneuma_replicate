78e0bb2ca77d9461802ba28cd80d4930d7e21df5
David Alexander <david.alexander01@ui.ac.id>
2024-07-12 15:00:14 +0700

    add hash checking to table registration

--- modules/registration/registration.py
+++ modules/registration/registration.py
@@ -14,9 +14,12 @@ class Registration:
             """CREATE OR REPLACE TABLE table_status (
                 id VARCHAR PRIMARY KEY,
                 table_name VARCHAR NOT NULL,
+                status VARCHAR NOT NULL,
+                time_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                creator VARCHAR NOT NULL,
+                hash VARCHAR NOT NULL,
                 context_count INTEGER NOT NULL DEFAULT 0 CHECK (context_count >= 0),
-                summary_count INTEGER NOT NULL DEFAULT 0 CHECK (summary_count >= 0),
-                status VARCHAR,
+                summary_count INTEGER NOT NULL DEFAULT 0 CHECK (summary_count >= 0)
                 )
             """
         )
@@ -47,21 +50,36 @@ class Registration:
 
     def read_csv(self, csv_path: str):
         name = csv_path.split("/")[-1][:-4]
-        self.connection.sql(
-            f"""CREATE TABLE {name} AS
-            FROM read_csv(
+        table = self.connection.sql(
+            f"""SELECT *
+                FROM read_csv(
                 '{csv_path}',
                 auto_detect=True,
                 header=True
             )"""
         )
 
+        table_hash = self.connection.sql(
+            f"""SELECT md5(string_agg(tbl::text, ''))s
+                FROM read_csv(
+                '{csv_path}',
+                auto_detect=True,
+                header=True
+            ) AS tbl"""
+        ).fetchone()[0]
+
+        # Check if table with the same hash already exist
+        if self.connection.sql(
+            f"SELECT * FROM table_status WHERE hash = '{table_hash}'"
+        ).fetchone():
+            return f"This table already exists in the database."
+
         self.connection.sql(
-            f"""INSERT INTO table_status (id, table_name, status)
-            VALUES ('{csv_path}', '{name}', 'ready')"""
+            f"""INSERT INTO table_status (id, table_name, status, creator, hash)
+            VALUES ('{csv_path}', '{name}', 'ready', 'fake creator', '{table_hash}')"""
         )
 
-        return f"Table ID: {csv_path}"
+        return f"Table with ID: {csv_path} has been added to the database."
 
     def add_context(self, table_id: str, context_path: str):
         with open(context_path, "r") as f:
