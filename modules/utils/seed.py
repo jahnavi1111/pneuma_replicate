d382d13b982a9d7b29574c3a02e1ea50c0f89c3c
David Alexander <david.alexander01@ui.ac.id>
2024-07-29 14:22:52 +0700

    restructure sample data directory

--- modules/utils/seed.py
+++ modules/utils/seed.py
@@ -11,19 +11,19 @@ if __name__ == "__main__":
     os.makedirs("../out", exist_ok=True)
     registration = Registration(DB_PATH)
     registration.setup()
-    registration.read_table("../sample_data/5cq6-qygt.csv", "david")
-    registration.read_table("../sample_data/5n77-2d6a.csv", "david")
+    registration.read_table("../sample_data/csv/5cq6-qygt.csv", "david")
+    registration.read_table("../sample_data/csv/5n77-2d6a.csv", "david")
 
     registration.add_context(
-        "../sample_data/5cq6-qygt.csv", "../sample_data/sample_context.txt"
+        "../sample_data/csv/5cq6-qygt.csv", "../sample_data/context/sample_context.txt"
     )
     registration.add_context(
-        "../sample_data/5n77-2d6a.csv", "../sample_data/sample_context.txt"
+        "../sample_data/csv/5n77-2d6a.csv", "../sample_data/context/sample_context.txt"
     )
 
     registration.add_summary(
-        "../sample_data/5cq6-qygt.csv", "../sample_data/sample_summary.txt"
+        "../sample_data/csv/5cq6-qygt.csv", "../sample_data/summary/sample_summary.txt"
     )
     registration.add_summary(
-        "../sample_data/5n77-2d6a.csv", "../sample_data/sample_summary.txt"
+        "../sample_data/csv/5n77-2d6a.csv", "../sample_data/summary/sample_summary.txt"
     )
