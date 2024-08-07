a1d141f34cd697d83c77c1257735ec5260107139
luthfibalaka <luthfibalaka@gmail.com>
2024-08-07 11:45:31 +0000

    improve pipeline tools

--- benchmark_generator/context/utils/pipeline_initializer.py
+++ benchmark_generator/context/utils/pipeline_initializer.py
@@ -1,19 +1,28 @@
+from torch import dtype
 from transformers import pipeline
 
 
-def initialize_pipeline(model_path: str, torch_dtype, context_length=8192, hf_token=""):
+def initialize_pipeline(
+    model_path: str, torch_dtype: dtype, context_length=8192, hf_token=""
+):
     """
     Initialize a text generation pipeline
 
     ### Parameters:
-    - model_path (str): The path of a model and tokenizer's weights
+    - model_path (str): The path of a model for the pipeline
+    - torch_dtype (dtype): The data type of the model
+    - context_length (int): The context length of the model
+    - hf_token (str): HuggingFace token to access gated model
 
     ### Returns:
     - pipe (TextGenerationPipeline): The pipeline for text generation
-    - hf_token (str): HuggingFace token to access gated model
     """
     pipe = pipeline(
-        "text-generation", model=model_path, device_map="auto", torch_dtype=torch_dtype, token=hf_token
+        "text-generation",
+        model=model_path,
+        device_map="auto",
+        torch_dtype=torch_dtype,
+        token=hf_token,
     )
     pipe.tokenizer.model_max_length = context_length
     return pipe
@@ -23,7 +32,7 @@ if __name__ == "__main__":
     import torch
     from transformers.pipelines.text_generation import TextGenerationPipeline
 
-    model_path = "tiny_llama"  # Adjust model path
+    model_path = "meta-llama/Meta-Llama-3-8B-Instruct"
     pipe = initialize_pipeline(model_path, torch.bfloat16)
 
     assert type(pipe) == TextGenerationPipeline
--- benchmark_generator/context/utils/prompting_interface.py
+++ benchmark_generator/context/utils/prompting_interface.py
@@ -1,25 +1,56 @@
-from transformers import set_seed
 import logging
+
+from transformers import set_seed
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase
+from transformers.pipelines.text_generation import TextGenerationPipeline
 
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger("prompting_interface.py")
 
-def truncate_text(tokenizer, conversation, context_length: int, max_new_tokens: int):
+
+def truncate_text(
+    tokenizer: PreTrainedTokenizerBase,
+    conversation: list[dict[str, str]],
+    context_length: int,
+    max_new_tokens: int,
+):
+    """
+    Truncate the text of a conversation according to context_length and max_new_tokens
+
+    ### Parameters:
+    - tokenizer (PreTrainedTokenizerBase): A PreTrainedTokenizerBase from HuggingFace
+    - conversation (list[dict[str, str]]): A conversation
+    - context_length (int): The context length of a model
+    - max_new_tokens (int): Max new tokens to be generated
+
+    ### Returns:
+    - text (str): The truncated text
+    """
     base = [{"role": "user", "content": ""}]
     base_len = len(
-        tokenizer.apply_chat_template(
-            base, tokenize=True, add_generation_prompt=True
-        )
+        tokenizer.apply_chat_template(base, tokenize=True, add_generation_prompt=True)
     )
     max_tokens = context_length - base_len - max_new_tokens
     tokens = tokenizer.tokenize(conversation[0]["content"])[:max_tokens]
     return tokenizer.convert_tokens_to_string(tokens)
 
 
-def is_within_context_length(tokenizer, conversation, context_length: int):
+def is_within_context_length(
+    tokenizer: PreTrainedTokenizerBase,
+    conversation: list[dict[str, str]],
+    context_length: int,
+):
     """
-    Check whether a conversation is within a context length
+    Check whether a conversation is within context_length
+
+    ### Parameters:
+    - tokenizer (PreTrainedTokenizerBase): A PreTrainedTokenizerBase from HuggingFace
+    - conversation (list[dict[str, str]]): A conversation
+    - context_length (int): The context length of a model
+
+    ### Returns:
+    - check_result (bool): Indicator of whether the check result is True
     """
     conv_len = len(
         tokenizer.apply_chat_template(
@@ -29,7 +60,13 @@ def is_within_context_length(tokenizer, conversation, context_length: int):
     return conv_len <= context_length
 
 
-def validate_generation_configs(generation_configs):
+def remove_unset_generation_configs(generation_configs: dict[str, any]):
+    """
+    Check whether a conversation is within context_length
+
+    ### Parameters:
+    - generation_configs (dict[str, any]): A dictionary of generation configs
+    """
     if generation_configs["top_k"] == 0:
         del generation_configs["top_k"]
     if generation_configs["top_p"] == 1.0:
@@ -41,8 +78,9 @@ def validate_generation_configs(generation_configs):
 
 
 def prompt_pipeline(
-    pipe,
-    conversation,
+    pipe: TextGenerationPipeline,
+    conversations: list[list[dict[str, str]]],
+    batch_size=2,
     context_length=8192,
     max_new_tokens=512,
     do_sample=False,
@@ -56,7 +94,8 @@ def prompt_pipeline(
 
     ### Parameters:
     - pipe (TextGenerationPipeline): An initialized pipeline.
-    - conversation (list[dict[str, str]]): The data type of the model
+    - conversations (list[list[dict[str, str]]]): The data type of the model
+    - batch_size (int): The batch size to process conversations
     - context_length (int): The LLM's context length
     - max_new_tokens (int): Max number of tokens generated for each prompt
     - do_sample (bool): Perform sampling or not
@@ -66,7 +105,7 @@ def prompt_pipeline(
     - temperature (float): Control how sharp the distribution (smaller means sharper)
 
     ### Returns:
-    - conversation (list[dict[str, str]]): The conversation appended with the model's output
+    - conversations (list[list[dict[str, str]]]): The conversations appended with the model's outputs
     """
     generation_configs = {
         "max_new_tokens": max_new_tokens,
@@ -77,29 +116,36 @@ def prompt_pipeline(
         "temperature": temperature,
         "pad_token_id": pipe.tokenizer.eos_token_id,
     }
-    validate_generation_configs(generation_configs)
+    remove_unset_generation_configs(generation_configs)
     try:
-        if not is_within_context_length(pipe.tokenizer, conversation, context_length):
-            truncated_prompt = truncate_text(pipe.tokenizer, conversation, context_length, max_new_tokens)
-            conversation[0]["content"] = truncated_prompt
+        for conversation in conversations:
+            if not is_within_context_length(
+                pipe.tokenizer, conversation, context_length
+            ):
+                truncated_text = truncate_text(
+                    pipe.tokenizer, conversation, context_length, max_new_tokens
+                )
+                conversation[0]["content"] = truncated_text
         set_seed(42)  # Enhance reproducibility for when using sampling
-        conversation = pipe(conversation, truncation=True, **generation_configs)[0]["generated_text"]
-        return conversation
-    except:
-        logger.warning(
-            "Something wrong is happening. Skip processing."
+        answers = pipe(
+            conversation, truncation=True, batch_size=batch_size, **generation_configs
         )
+        conversations = []
+        for answer in answers:
+            conversations.append(answer[0]["generated_text"])
+        return conversations
+    except Exception as error:
+        logger.warning(error)
         return [{"role": "user", "content": ""}]
 
 
 if __name__ == "__main__":
-    # Try to prompt a pipeline
     import torch
     from pipeline_initializer import initialize_pipeline
 
-    pipe = initialize_pipeline("tiny_llama", torch.bfloat16)  # Adjust model path & data type
-    conversation = [{"role": "user", "content": "Tell me about Illinois!"}]
-    output = prompt_pipeline(pipe, conversation, 2048)
+    pipe = initialize_pipeline("meta-llama/Meta-Llama-3-8B-Instruct", torch.bfloat16)
+    conversations = [[{"role": "user", "content": "Tell me about Illinois!"}]]
+    output = prompt_pipeline(pipe, conversations, 1, 8192)
 
     assert len(output) == 2
-    assert output[-1]["role"] == "assistant"
+    assert output[0][-1]["role"] == "assistant"
