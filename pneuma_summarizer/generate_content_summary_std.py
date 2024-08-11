38e5c1600dfbdbd49b6fe87c121b2c7cc11fb763
luthfibalaka <luthfibalaka@gmail.com>
2024-08-11 09:45:15 +0000

    enable batch inference to summarizer

--- pneuma_summarizer/generate_content_summary_std.py
+++ pneuma_summarizer/generate_content_summary_std.py
@@ -1,6 +1,6 @@
 import os
 import json
-import duckdb
+import pandas as pd
 
 from tqdm import tqdm
 
@@ -12,24 +12,19 @@ def write_jsonl(data: list[dict[str, str]], file_path: str):
             file.write("\n")
 
 
-con = duckdb.connect()
-tables_path = "../data_src/tables/pneuma_public_bi"
+tables_path = "pneuma_fetaqa"
+summaries_name = "fetaqa_standard.jsonl"
 content_summaries: list[dict[str, str]] = []
 tables = [
     table[:-4] for table in sorted(os.listdir(tables_path)) if table.endswith(".csv")
 ]
 
 for table in tqdm(tables, "processing tables"):
-    df = (
-        con.sql(f"select * from '{tables_path}/{table}.csv'")
-        .to_df()
-        .drop_duplicates()
-        .reset_index(drop=True)
-    )
+    df = pd.read_csv(f"{tables_path}/{table}.csv")
     summary = {
         "table": table,
         "summary": " | ".join(df.columns),
     }
     content_summaries.append(summary)
 
-write_jsonl(content_summaries, "summaries/standard/public_standard.jsonl")
+write_jsonl(content_summaries, f"summaries/standard/{summaries_name}")
