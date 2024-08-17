886e846db17e65f39cc16de8eca04ce7fab25ae7
luthfibalaka <luthfibalaka@gmail.com>
2024-08-17 13:39:46 +0000

    ensure reproducibility of llm summarizer

--- pneuma_summarizer/generate_content_summary_llm.py
+++ pneuma_summarizer/generate_content_summary_llm.py
@@ -1,31 +1,26 @@
 import os
+
+os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+import setproctitle
+
+setproctitle.setproctitle("python")
+
 import sys
-import json
-import pandas as pd
 import torch
+import pandas as pd
 
 sys.path.append("..")
 
 from tqdm import tqdm
 from benchmark_generator.context.utils.pipeline_initializer import initialize_pipeline
 from benchmark_generator.context.utils.prompting_interface import prompt_pipeline
-
-# Select GPU (if necessary)
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-import setproctitle
-setproctitle.setproctitle("python")
+from benchmark_generator.context.utils.jsonl import write_jsonl, read_jsonl
 
 pipe = initialize_pipeline("meta-llama/Meta-Llama-3-8B-Instruct", torch.bfloat16)
 # Specific setting for Llama-3-8B-Instruct for batching
 pipe.tokenizer.pad_token_id = pipe.model.config.eos_token_id
-pipe.tokenizer.padding_side = 'left'
-
-
-def write_jsonl(data: list[dict[str, str]], file_path: str):
-    with open(file_path, "w", encoding="utf-8") as file:
-        for item in data:
-            file.write(json.dumps(item))
-            file.write("\n")
+pipe.tokenizer.padding_side = "left"
 
 
 def get_col_description_prompt(columns: str, column: str):
@@ -38,33 +33,47 @@ Describe briefly what the {column} column represents. If not possible, simply st
 
 def generate_descriptions(src_path: str, descriptions_path: str):
     tables = sorted([file[:-4] for file in os.listdir(src_path)])
-    summaries: list[dict[str, str]] = []
+    try:
+        summaries = read_jsonl(descriptions_path)
+    except:
+        summaries: list[dict[str, str]] = []
 
     bar = tqdm(tables)
     for table in bar:
+        # Skip already summarized tables
+        if table in [summary["table"] for summary in summaries]:
+            continue
         bar.set_description(f"Processing table {table}")
-        df = pd.read_csv(f"{src_path}/{table}.csv")
+        df = pd.read_csv(f"{src_path}/{table}.csv", on_bad_lines="skip")
         cols = df.columns
         conversations = []
         for col in cols:
             prompt = get_col_description_prompt(" | ".join(cols), col)
             conversations.append([{"role": "user", "content": prompt}])
         
-        for i in tqdm(range(0, len(conversations), 3)):
+        if len(conversations) > 0:
             outputs = prompt_pipeline(
-                pipe, conversations[i:i+3], batch_size=3, context_length=8192, max_new_tokens=400, temperature=None, top_p=None
+                pipe,
+                conversations,
+                batch_size=2,
+                context_length=8192,
+                max_new_tokens=400,
+                temperature=None,
+                top_p=None,
             )
+
+            col_narrations: list[str] = []
             for output_idx, output in enumerate(outputs):
-                summary = output[-1]["content"]
-                new_row = {
-                    "table": table,
-                    "summary": f"{cols[i+output_idx]}: {summary}",
-                }
-                summaries.append(new_row)
-                write_jsonl(summaries, f"{descriptions_path}")
+                col_narrations.append(f"{cols[output_idx]}: {output[-1]["content"]}")
+            
+            summaries.append({
+                "table": table,
+                "summary": " | ".join(col_narrations)
+            })
+            write_jsonl(summaries, descriptions_path)
 
 
 # Adjust paths
-src_path = "pneuma_fetaqa"
-descriptions_path = "summaries/narrations/fetaqa_narrations.jsonl"
+src_path = "../data_src/tables/pneuma_public_bi"
+descriptions_path = "summaries/narrations/public_narrations.jsonl"
 generate_descriptions(src_path, descriptions_path)
