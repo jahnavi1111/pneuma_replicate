f1f5b6a623b6fb0a8c0fbe1baa275a8e83ffc1b0
luthfibalaka <luthfibalaka@gmail.com>
2025-01-06 02:46:32 +0000

    integrate dataset processing to downloader

--- data_src/tables/downloader.py
+++ data_src/tables/downloader.py
@@ -7,23 +7,9 @@ from tqdm import tqdm
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-def extract_tar(file_path, extract_path="."):
-    with tarfile.open(file_path, "r") as tar:
-        tar.extractall(path=extract_path)
-        print(f"Extracted all files to '{extract_path}'")
-
-
-tables = [
-    "pneuma_public_bi.tar",
-    "pneuma_chicago_10K.tar",
-    "pneuma_chembl_10K.tar",
-    "pneuma_fetaqa.tar",
-    "pneuma_adventure_works.tar",
-]
-
-for table in tqdm(tables):
-    urllib.request.urlretrieve(
-        f"https://storage.googleapis.com/pneuma_open/{table}",
-        filename=os.path.join(script_dir, table),
-    )
-    extract_tar(os.path.join(script_dir, table), script_dir)
+def post_process_dataset(dataset_path: str):
+    for table in os.listdir(dataset_path):
+        os.rename(
+            f"{dataset_path}/{table}", f"{dataset_path}/{table.split('_SEP_')[1]}"
+        )
+    print("Processed the dataset")
